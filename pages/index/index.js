0913cdbb1e746801a169f674d72a1bc01c2cab46
HankinsPan <hankins.home@outlook.com>
2018-01-20 12:29:11 +0800

    modify swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,25 +5,23 @@ var utils = require('../../utils/util.js')
 
 Page({
   data: {
-    mode: 'scaleToFill',
-    src: '../../images/ic_arrow_down.png'
-  },
-
-
-  loadMore(e) {
-  },
-
-  getNextDate() {
-    const now = new Date()
-    now.setDate(now.getDate() - this.index++)
-
-    console.log('getNextDate ->', now);
-
-    return now
+    mode: "scaleToFill",
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 2000,
   },
 
   onLoad() {
-    let that = this
+    var array = this.data.imgUrls;
 
+    console.log("fuck imgUrls ->", this.data.imgUrls);
   }
+
+
 })
